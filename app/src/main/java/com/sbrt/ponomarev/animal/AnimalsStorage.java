ac2b946d6ff66c457e72bb4e7facf9fddcd5ef9c
Ponomarev A. (work) <gpwaic@gmail.com>
2017-05-22 15:15:11 +0300

    fourth commit

--- app/src/main/java/com/sbrt/ponomarev/animal/AnimalsStorage.java
+++ app/src/main/java/com/sbrt/ponomarev/animal/AnimalsStorage.java
@@ -2,6 +2,7 @@ package com.sbrt.ponomarev.animal;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -11,7 +12,7 @@ import java.util.List;
 public class AnimalsStorage {
 
     //    private final List<Animal> sAnimals = new ArrayList<>();
-    private static final List<Animal> sAnimals = Arrays.asList(
+    private static List<Animal> sAnimals = Collections.unmodifiableList(Arrays.asList(
             new Animal("Bob", "Dog", 10f, 120f),
             new Animal("Jack", "Cat", 1.2f, 10f),
             new Animal("Mercy", "Cat", 9f, 60f),
@@ -20,7 +21,7 @@ public class AnimalsStorage {
             new Animal("Moris", "Dog", 50f, 150f),
             new Animal("Benny", "Horse", 450.5f, 250f),
             new Animal("Milka", "Caw", 623.4f, 90.5f)
-    );
+    ));
     private List<Animal> mAnimals;
     private List<OnContentChangeListener> mOnContentChangeListeners = new ArrayList<>();
 
